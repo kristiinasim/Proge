5ba8f778dcc64b3edb1486fd750ba46c80da451b
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2019-09-17 15:48:55 +0300

    Removed one thing

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using WebApplication1.Views.Home;
+
 
 namespace WebApplication1
 {
